738f06a0dfce1fe09ba62b54e8e3a90f07402d4d
maximecornaton <maxime.cornaton@cpe.fr>
2023-04-25 14:36:46 +0200

    oui

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -3,18 +3,24 @@
  */
 package model;
 
+import java.util.List;
+
 /**
  * @author maxime
  *
  */
 public class Jeu {
 	
+	private Couleur couleur;
+	private List<Pieces> Pieces;
+	
 	/**
 	 * Le constructeur de jeu fait appel à la fabrique de pièces
 	 * @param color
 	 */
-	public Jeu(Couleur color) {
-		
+	public Jeu(Couleur couleur) {
+		this.couleur = couleur;
+		this.Pieces = tools.ChessPiecesFactory.newPieces(couleur);
 	}
 	
 	
