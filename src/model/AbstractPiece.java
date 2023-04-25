4093f13737ebc130ed468071f92c0590cc03d5c0
rodologie <91213039+rodologie@users.noreply.github.com>
2023-04-25 14:01:08 +0200

    COMPLETETION - AbstractPiece

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -9,7 +9,7 @@ public abstract class AbstractPiece {
 	protected Coord coord;
 	private Couleur couleur;
 
-	public AbstractPiece(Couleur couleur, Coord coord) {
+	public AbstractPiece(Couleur couleur) {
 		this.coord = new Coord(0, 0);
 		this.couleur=couleur;
 	}
@@ -26,19 +26,19 @@ public abstract class AbstractPiece {
 		return this.couleur;
 	}
 	
-	public boolean move() {
-		return true;
+	/**public boolean move() {
 	}
 	
 	public boolean capture() {
-		return true;
-	}
+		
+	}*/
 	
 	@Override
 	public String toString() {
-		return null;
+		return this.getClass().getSimpleName() + " (" + this.getX() + ";" + this.getY() + ")";
 	}
 	
 	public abstract boolean isMoveOk(int xFinal, int yFinal);
 	
 }
+	
