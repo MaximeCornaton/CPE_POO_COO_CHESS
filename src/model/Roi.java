6e5b61bb5e52a118ca6d625cb3ff44a1bae073b7
valbrqd <valbrqd@gmail.com>
2023-04-25 14:43:03 +0200

    fou roi

--- src/model/Roi.java
+++ src/model/Roi.java
@@ -1,5 +1,16 @@
 package model;
 
-public class Roi {
+public class Roi extends AbstractPiece{
+	
+	public Roi(Couleur couleur, Coord coord) {
+		super(couleur,coord);
+		
+	}
 
+	@Override
+	public boolean isMoveOk(int xFinal, int yFinal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
