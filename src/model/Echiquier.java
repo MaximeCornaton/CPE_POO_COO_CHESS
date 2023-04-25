3a321c078cf26a7239542412d632ed48f99df0a9
valbrqd <valbrqd@gmail.com>
2023-04-25 16:40:28 +0200

    ok

--- src/model/Echiquier.java
+++ src/model/Echiquier.java
@@ -1,14 +1,34 @@
 package model;
 
 public class Echiquier implements BoardGames{
+	private Jeu noir;
+	private Jeu blanc;
+	private Jeu courant;
+	private Jeu non_courant;
+	
 	
 	private Echiquier() {
+		this.blanc = new Jeu(Couleur.BLANC);
+		this.noir = new Jeu(Couleur.NOIR);
+		this.courant = this.blanc;
+		this.non_courant = this.noir;
+	}
+	/**
+	 * 
+	 * @param xInit
+	 * @param yInit
+	 * @param xFinal
+	 * @param yFinal
+	 * @return Permet de vérifier si une pièce peut être déplacé
+	 */
+	private boolean isMoveOk(int xInit, int yInit, int xFinal, int yFinal) {
 		
 	}
 
 	@Override
 	public boolean move(int xInit, int yInit, int xFinal, int yFinal) {
 		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
