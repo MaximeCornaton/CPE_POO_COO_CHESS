8aab9b4b5c389ff770f8d6216917dcc34005a1c9
valbrqd <valbrqd@gmail.com>
2023-04-25 15:32:13 +0200

    ok

--- src/model/Reine.java
+++ src/model/Reine.java
@@ -6,5 +6,18 @@ public class Reine extends AbstractPiece{
 		super(couleur,coord);
 	}
 	
+	public boolean isMoveOk(int xFinal, int yFinal) {
+		return (xFinal >= 0 && yFinal >= 0 && xFinal != this.getX() && yFinal == this.getY() || xFinal == this.getX() 
+					&& yFinal != this.getY() && super.isMoveOk(xFinal, yFinal) || Math.abs(yFinal - this.getY()) == Math.abs(xFinal) - this.getX());
+	}
 	
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Reine a = new Reine(Couleur.NOIR, new Coord(0,0));
+		System.out.println(a.isMoveOk(7, 5));
+		
+		System.out.println(a);
+		
+	}
 }
