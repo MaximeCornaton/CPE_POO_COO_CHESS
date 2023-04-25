1b08192c6d69757a1daf7ccc18df0f6cad49f960
maximecornaton <maxime.cornaton@cpe.fr>
2023-04-25 14:13:19 +0200

    oui

---
+++ src/model/Jeu.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author maxime
+ *
+ */
+public class Jeu {
+	
+	/**
+	 * Le constructeur de jeu fait appel à la fabrique de pièces
+	 * @param color
+	 */
+	public Jeu(Couleur color) {
+		
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
